3442b73e4b05f62e94aa8ff01f0edaed3165e4c3
Jayesh Singh <jayeshsingh881@gmail.com>
2026-02-11 03:58:09 +0530

    fix(auth): use local payload auth with type cast

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,28 +1,26 @@
 import { User } from '../payload-types'
 import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
 import { NextRequest } from 'next/server'
+import { getPayloadClient } from '../get-payload'
 
 export const getServerSideUser = async (
   cookies: NextRequest['cookies'] | ReadonlyRequestCookies
 ) => {
   const token = cookies.get('payload-token')?.value
 
-  try {
-    const meRes = await fetch(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
-      {
-        headers: {
-          Authorization: `JWT ${token}`,
-        },
-      }
-    )
+  if (!token) return { user: null }
 
-    const { user } = (await meRes.json()) as {
-      user: User | null
-    }
+  try {
+    const payload = await getPayloadClient()
+    const { user } = await (payload as any).auth({
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    })
 
     return { user }
   } catch (err) {
+    // console.error(err)
     return { user: null }
   }
 }
