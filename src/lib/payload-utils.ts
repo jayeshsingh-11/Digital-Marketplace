6db1b41e625154ab90b788bf0c10d2b2d5eb1539
Jayesh Singh <jayeshsingh881@gmail.com>
2026-02-16 08:19:32 +0530

    Fix missing module import in payload-utils.ts

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,6 +1,6 @@
 import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
 import { NextRequest } from 'next/server'
-import { getServerSideUserNode } from './payload-utils-node'
+import { getServerSideUserNode } from './auth-utils'
 
 export const getServerSideUser = async (
   cookies: NextRequest['cookies'] | ReadonlyRequestCookies
