5ccb4b7023ed2b87c3dcdf4ac585842722fead49
Jayesh Singh <jayeshsingh881@gmail.com>
2026-02-16 22:45:15 +0530

    Update metadataBase to Vercel URL

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -59,7 +59,7 @@ export function constructMetadata({
       creator: '@joshtriedcoding',
     },
     icons,
-    metadataBase: new URL('https://digitalhippo.up.railway.app'),
+    metadataBase: new URL('https://digital-marketplace-ecru.vercel.app'),
     ...(noIndex && {
       robots: {
         index: false,
